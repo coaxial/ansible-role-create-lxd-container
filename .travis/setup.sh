8bb3810979efb02f3b3fd773a0c7a251acafd81f
coaxial <hi@64b.it>
2018-08-05 13:48:36 -0400

    fixup! fixup! Restart lxd after changing uid and gid maps

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -16,6 +16,10 @@ sudo sed -i 's/^\(lxd:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subuid
 sudo sed -i 's/^\(root:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subuid
 sudo sed -i 's/^\(lxd:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subgid
 sudo sed -i 's/^\(root:[0-9]\{1,\}\):.*/\1:1000000000/' /etc/subgid
+cat /etc/subuid
+cat /etc/subgid
 sudo systemctl restart lxd
+cat /etc/subuid
+cat /etc/subgid
 
 cat /var/log/lxd/lxd.log
