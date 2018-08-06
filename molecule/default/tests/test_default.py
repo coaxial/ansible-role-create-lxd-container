e075013b8391f75e6170b20f4208159a14b997ae
coaxial <hi@64b.it>
2018-08-06 15:13:18 -0400

    Test defaults override

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -17,8 +17,8 @@ def test_example(host):
     assert file.group == 'root'
 
 
-def test_container_creation(host):
-    c = yaml.load(host.check_output('lxc config show test-container'))
+def test_container_creation_defaults(host):
+    c = yaml.load(host.check_output('lxc config show test-container-1'))
 
     assert strtobool(c['config']['boot.autostart'])
     assert c['config']['limits.cpu'] == ""
@@ -30,3 +30,20 @@ def test_container_creation(host):
     assert c['devices']['eth0']['parent'] == 'lxdbr0'
     assert c['devices']['eth0']['ipv4.address'] == 'auto'
     assert c['devices']['eth0']['ipv6.address'] == 'auto'
+    assert c['config']['image.version'] == '18.04'
+
+
+def test_container_creation_override_defaults(host):
+    c = yaml.load(host.check_output('lxc config show test-container-2'))
+
+    assert not strtobool(c['config']['boot.autostart'])
+    assert c['config']['limits.cpu'] == "1"
+    assert c['config']['limits.cpu.allowance'] == '90%'
+    assert c['config']['limits.memory'] == "512MB"
+    assert strtobool(c['config']['security.nesting'])
+    assert strtobool(c['config']['security.privileged'])
+    assert c['profiles'] == ['default']
+    assert c['devices']['eth0']['parent'] == 'lxdbr0'
+    assert c['devices']['eth0']['ipv4.address'] == '10.100.12.10'
+    assert c['devices']['eth0']['ipv6.address'] == 'auto'
+    assert c['config']['image.version'] == '16.04'
