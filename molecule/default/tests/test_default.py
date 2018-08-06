8c8e29ac4bef4bd1c8bd8b9ee9f67c598715b282
coaxial <hi@64b.it>
2018-08-06 13:48:07 -0400

    Add tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,6 +2,9 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import yaml
+from distutils.util import strtobool
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
@@ -12,3 +15,18 @@ def test_example(host):
     assert file.exists
     assert file.user == 'root'
     assert file.group == 'root'
+
+
+def test_container_creation(host):
+    c = yaml.load(host.check_output('lxc config show test-container'))
+
+    assert strtobool(c['config']['boot.autostart'])
+    assert c['config']['limits.cpu'] == ""
+    assert c['config']['limits.cpu.allowance'] == '100%'
+    assert c['config']['limits.memory'] == ""
+    assert not strtobool(c['config']['security.nesting'])
+    assert not strtobool(c['config']['security.privileged'])
+    assert c['profiles'] == ['default']
+    assert c['devices']['eth0']['parent'] == 'lxdbr0'
+    assert c['devices']['eth0']['ipv4.address'] == 'auto'
+    assert c['devices']['eth0']['ipv6.address'] == 'auto'
